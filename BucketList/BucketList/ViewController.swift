b2d2dd22b3f22fc7f27b23490f731c24a415763a
Torry Yang <ty5kj@labmini08.lab.cs.virginia.edu>
2016-09-25 15:50:20 -0400

    Made task list main storyboard

--- BucketList/BucketList/ViewController.swift
+++ BucketList/BucketList/ViewController.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var taskView: UITableView!
+    
+    var tasks = ["Do laundry", "Finish this project", "Call Mum", "Find girlfriend"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.taskView.dataSource = self
+        self.taskView.delegate = self
     }
-
-    
-    
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
     }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = self.tasks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            self.tasks.remove(at: indexPath.row)
+            self.taskView.reloadData()
+        }
+    }
+    
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
